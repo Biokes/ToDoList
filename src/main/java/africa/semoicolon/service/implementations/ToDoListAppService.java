af8761fb14462e46418db12a9c2a19804de7ad18
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-01 23:59:43 +0100

    create todolist register user

--- src/main/java/africa/semoicolon/service/implementations/ToDoListAppService.java
+++ src/main/java/africa/semoicolon/service/implementations/ToDoListAppService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AppServiceService implements AppService {
+public class TODoListAppService implements AppService {
     @Autowired
     private UserService userService;
     @Autowired
