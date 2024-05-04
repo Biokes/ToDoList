605aa46f5e8f6853c40d815b802eb977e59c9a0b
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-04 10:45:09 +0100

    update end points

--- src/main/java/africa/semoicolon/controller/ToDoListController.java
+++ src/main/java/africa/semoicolon/controller/ToDoListController.java
@@ -1,26 +1,22 @@
 package africa.semoicolon.controller;
 
 
-import africa.semoicolon.dtos.request.RegisterRequest;
+import africa.semoicolon.dtos.request.*;
 import africa.semoicolon.dtos.response.ApiResponse;
 import africa.semoicolon.exceptions.ToDoListException;
 import africa.semoicolon.service.inferaces.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("api/v1/toDoList/")
 public class ToDoListController{
     @Autowired
     private AppService appService;
-    @PostMapping()
+    @PostMapping("/Register")
     public ResponseEntity<?> register(@RequestBody RegisterRequest request){
         try{
             appService.register(request);
@@ -30,15 +26,82 @@ public class ToDoListController{
             return new ResponseEntity<>(new ApiResponse(false,exception.getMessage()), BAD_REQUEST);
         }
     }
+    @DeleteMapping("/Delete_account")
+    public ResponseEntity<?> deActivateAccount(@RequestBody DeleteUserRequest delete){
+        try{
+            appService.deleteAccount(delete);
+            return new ResponseEntity<>(new ApiResponse(true,delete.getUsername()+"deleted successfully"),OK);
+        }
+        catch(ToDoListException error){
+            return new ResponseEntity<>(new ApiResponse(false, error.getMessage()),BAD_REQUEST);
+
+        }
+    }
+    @PutMapping("/Create-task")
+    public ResponseEntity<?> createTask(@RequestBody CreateTaskRequest create){
+        try{
+            return new ResponseEntity<>(new ApiResponse(true,appService.createTask(create)), OK);
+        }
+        catch(ToDoListException exception){
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()),BAD_REQUEST);
+        }
+    }
+    @DeleteMapping("/delete_task")
+    public ResponseEntity<?> deleteTask(@RequestBody DeleteTaskRequest delete){
+        try{
+            appService.deleteTask(delete);
+            return new ResponseEntity<>((new ApiResponse(false,delete.getTaskName()+" deleted successfully.")),OK);
+        }
+        catch(ToDoListException error){
+            return new ResponseEntity<>(new ApiResponse(false, error.getMessage()),BAD_REQUEST);
+        }
+    }
+    @PatchMapping("/update-task")
+    public ResponseEntity<?> updateTask(@RequestBody UpdateTaskRequest update){
+        try{
+            return new ResponseEntity<>(new ApiResponse(true,appService.updateTask(update)),OK);
+        }
+        catch(ToDoListException error){
+            return new ResponseEntity<>(new ApiResponse(false, error.getMessage()),BAD_REQUEST);
+        }
+    }
+    @PatchMapping("/Complete-Task")
+    public ResponseEntity<?> completeTask(@RequestBody CompleteTaskRequest complete){
+        try{
+           return new ResponseEntity<>(new ApiResponse(true,appService.completeTask(complete)),OK);
+        }
+        catch(ToDoListException error){
+            return new ResponseEntity<>(new ApiResponse(false,error.getMessage()),BAD_REQUEST);
+        }
+    }
+    @PostMapping("/assign-Task")
+    public ResponseEntity<?> assignTask(@RequestBody AssignTaskRequest assign){
+        try{
+            return new ResponseEntity<>(new ApiResponse(true,appService.assignTask(assign)),OK);
+        }
+        catch(ToDoListException error){
+            return new ResponseEntity<>(new ApiResponse(false,error.getMessage()),BAD_REQUEST);
+        }
+    }
+    @GetMapping("/logIn")
+    public ResponseEntity<?> login(@RequestBody LoginRequest login){
+        try{
+            return new ResponseEntity<>(new ApiResponse(true,appService.login(login)),OK);
+        }
+        catch(ToDoListException error){
+            return new ResponseEntity<>(new ApiResponse(false, error.getMessage()),BAD_REQUEST);
+        }
+    }
+    @GetMapping("/notifications")
+    public ResponseEntity<?> notifications(@RequestBody LoginRequest request){
+        try{
+            return new ResponseEntity<>(new ApiResponse(true,appService.viewNotifications(request)),OK);
+        }
+        catch(ToDoListException error){
+            return new ResponseEntity<>(new ApiResponse(false,error.getMessage()),BAD_REQUEST);
+        }
+    }
 }
-//register user
-// delete user
-//create task
-//delete task
-//update task
-// complete task
-// assign task
-//notify user after loggin in
 // view notifications
 //find all created tasks
 // find all pending tasks
