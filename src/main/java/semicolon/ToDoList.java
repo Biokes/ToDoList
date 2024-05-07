95a4b619926c19ea0baee3ef5c1bf16b18e81460
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-07 12:01:07 +0100

    add main

---
+++ src/main/java/semicolon/ToDoList.java
@@ -0,0 +1,11 @@
+package semicolon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoList{
+    public static void main(String[] args){
+        SpringApplication.run(ToDoList.class, args);
+    }
+}
